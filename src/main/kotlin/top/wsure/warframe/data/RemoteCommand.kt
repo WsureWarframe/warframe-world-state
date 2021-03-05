c3d514e4ef851ac960e21a013e927846f242b31f
WangShuo <wangshuo@wayyue.com>
2021-03-06 01:20:35 +0800

    commit

---
+++ src/main/kotlin/top/wsure/warframe/data/RemoteCommand.kt
@@ -0,0 +1,29 @@
+package top.wsure.warframe.data
+
+import kotlinx.serialization.Serializable
+
+/**
+ * 远程接口对应指令参数类
+ *
+ * 如果你想在本接口上改造，打造自己的远程指令，参考
+ *
+ * http://nymph.rbq.life:3000/robot/commands
+ *
+ * FileName: WFCommand
+ * Author:   wsure
+ * Date:     2021/3/4 8:21 下午
+ * Description:
+ */
+@Serializable
+data class RemoteCommand(
+    val name: String,
+    val type: CommandType,
+    val path: String,
+    val alia: String,
+    val desc: String,
+)
+
+enum class CommandType{
+    PARAM,
+    SIMPLE
+}
---
+++ src/main/kotlin/top/wsure/warframe/data/WorldStateData.kt
@@ -0,0 +1,25 @@
+package top.wsure.warframe.data
+
+import net.mamoe.mirai.console.data.AutoSavePluginConfig
+import net.mamoe.mirai.console.data.value
+import top.wsure.warframe.utils.CommandUtils
+
+/**
+ * FileName: WorldStateData
+ * Author:   wsure
+ * Date:     2021/3/4 8:15 下午
+ * Description:
+ */
+object WorldStateData : AutoSavePluginConfig("WorldStateData") {
+    //api host 默认使用自身的，可自己改
+    var host: String by value("http://nymph.rbq.life:3000")
+
+    //help key
+    var helpKey: String by value("help")
+
+    var commandList: List<RemoteCommand> by value(emptyList())
+
+    init {
+        commandList = CommandUtils.getRemoteCommand(host)
+    }
+}
